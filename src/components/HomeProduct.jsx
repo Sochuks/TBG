217cac482485aa18de7df6a51e59ee1f0b2be859
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2025-03-01 14:52:35 +0100

    Added store section and cta

--- src/components/HomeProduct.jsx
+++ src/components/HomeProduct.jsx
@@ -12,7 +12,7 @@ const HomeProduct = ({product}) =>{
     };
   
     return(
-        <div  className="group block overflow-hidden">
+        <a  className="group block overflow-hidden cursor-pointer">
             <div className="relative h-[350px] sm:h-[450px]">
                 <img
                 src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
@@ -28,24 +28,33 @@ const HomeProduct = ({product}) =>{
             </div>
 
             <div className="relative bg-white pt-3">
-                <h3 className="text-sm text-gray-700 group-hover:underline group-hover:underline-offset-4">
+                <h3 className="text-sm text-deep-olive group-hover:underline group-hover:underline-offset-4">
                 {product.name}
                 </h3>
                 <div className='flex justify-between'>
                 <p className="mt-1.5 tracking-wide text-gray-900">{product.actual_price}</p>
-
-                
-                <button
-                onClick={() => addToCart(product)}
-                
-                    className="block w-full rounded-sm bg-yellow-400 p-4 text-sm font-medium transition hover:scale-105"
-                >
-                    Add to Cart
-                </button>
+                <p className="mt-1.5 tracking-wide text-gray-900">SIZE: {product.size}</p>
    
                 </div>
+                {/* Cart Button */}
+                <button
+                    onClick={() => addToCart(product)}
+                    className="group mt-4 inline-flex items-center gap-1 font-medium text-blue-600"
+                >
+
+                    <span
+                    aria-hidden="true"
+                    className="block transition-all group-hover:ms-0.5 rtl:rotate-180"
+                    >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                    </svg>
+
+                    </span>
+                    <p>Add to Cart</p>
+                </button>
             </div>
-        </div>
+        </a>
     )
 };
 
